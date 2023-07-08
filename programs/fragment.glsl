57747ce463f8b3b4dd065ada208bde12e1b719fb
damjanh <damjan.hudobreznik@gmail.com>
2023-07-08 23:12:25 +0200

    Implement different color for objects.

--- programs/fragment.glsl
+++ programs/fragment.glsl
@@ -58,6 +58,19 @@ vec3 getLight(vec3 p, vec3 rd, vec3 color) {
     return diffuse;
 }
 
+vec3 getMaterial(vec3 p, float id) {
+    vec3 m;
+    switch (int(id)) {
+        case 1:
+        m = vec3(0.9, 0.9, 0.0);
+        break;
+        case 2:
+        m = vec3(0.0, 0.5, 0.5);
+        break;
+    }
+    return m;
+}
+
 void render(inout vec3 col, in vec2 uv) {
     vec3 ro = vec3(0.0, 0.0, -3.0);
     vec3 rd = normalize(vec3(uv, FOV));
@@ -66,7 +79,8 @@ void render(inout vec3 col, in vec2 uv) {
 
     if (object.x < MAX_DIST) {
         vec3 p = ro + object.x * rd;
-        col += getLight(p, rd, vec3(1));
+        vec3 material = getMaterial(p, object.y);
+        col += getLight(p, rd, material);
     }
 }
 
